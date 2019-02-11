954295a0eb4202bf8c9c6a52e96d5e28936fdc2c
RubberDuck55 <34989573+RubberDuck55@users.noreply.github.com>
2019-02-10 19:43:07 -0500

    Update sketch.js

--- oof/sketch.js
+++ oof/sketch.js
@@ -1,9 +1,52 @@
-//James Wood 1/16/2019
+//James Wood 2/10/2019 7:42
 //Writen in ES6
+var i;
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(1350, 650);
+}
+var cricle = [];
+function draw() {
+	fill(255 ,255, 255);
+	text("");
+	if(mouseIsPressed){
+	var newn = new ball();
+	//if (cricle.length === 20){
+	//cricle.splice(1,1);
+	//}
+	cricle.push(newn);
+}
+	background(20);
+	for(i = 0; i < cricle.length; i++){
+	 cricle[i].Anime();
+	}
 }
 
-function draw() {
-  background(10);
+class ball {
+	constructor() {
+		this.x = mouseX;
+		this.y = mouseY;
+		this.xd = random(-10, 10);
+		this.yd = random(-10, 10);
+		this.size = floor(random(10,20));
+		this.thisx = 400-(size/2);
+		this.thissx = size/2;
+	}
+	Anime() {
+		fill(255,255,255,100);
+		ellipse(this.x, this.y, this.size, this.size);
+		if (this.x <= (width-10)) {
+			this.xd *= -1;
+		}
+		if (this.x >= 10) {
+			this.xd *= -1;
+		}
+		if (this.y <= (height-10)) {
+			this.yd *= -1;
+		} 
+		if (this.y >= 10) {
+			this.yd *= -1;
+		}
+		this.x+=this.xd
+		this.y+=this.yd
+	}
 }
