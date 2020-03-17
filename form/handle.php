cb3a8e36a35dcc8fec29e10d21dc0656d4f1e9fc
Jame <34989573+RubberDuck55@users.noreply.github.com>
2020-03-17 10:09:57 -0400

    Update handle.php

--- form/handle.php
+++ form/handle.php
@@ -1,7 +1,35 @@
-<html>
-<body>
 <?php
-echo Hello: $_POST['name'];
+$name = $_POST["name"];
 ?>
-</body>
+
 <html>
+<head>
+<title>Simple PHP Form Example</title>
+</head>
+
+<body>
+
+<?php
+if (!isset($_POST['submit']))
+{
+  // display the form
+  ?>
+  <p>
+  <form method="post" action="<?php echo $PHP_SELF;?>">
+  First Name: <input type="text" name="name">
+  <br/><input type="submit" value="submit" name="submit">
+  </form>
+  </p>
+
+<?
+}
+else
+{
+  echo "<p>";
+  echo "First Name: $name<br />";
+  echo "</p>";
+}
+?>
+
+</body>
+</html>
